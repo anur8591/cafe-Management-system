9af11500988c504e2b0dbbbb8dc35ed8b14bea85
Anurag Pal <palanurag595@gmail.com>
2025-10-05 19:03:10 +0530

    Cafe_Management Mini project is complete

--- Cafe_Management.py
+++ Cafe_Management.py
@@ -23,3 +23,14 @@ if item in menu:
 else:
     print(f"sorry, {item} is not available in the menu, please choose from the menu")
 
+another_order = input("Do you want to order another item? (yes/no):")
+if another_order.lower() == 'yes':
+    item = input("Enter the another item you want to order: ")
+    if item in menu:
+        total += menu[item]
+        print(f"{item} added to your order. price: {menu[item]}")
+    else:
+        print(f"sorry, {item} is not available in the menu, please choose from the menu")
+        
+print(f"Your total bill is: {total}")
+

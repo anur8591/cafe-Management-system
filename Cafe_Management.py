f132895b1015d94d4ee109533e1fe56ff48ecc4d
Anurag Pal <palanurag595@gmail.com>
2025-10-05 18:14:12 +0530

    taking first order

--- Cafe_Management.py
+++ Cafe_Management.py
@@ -8,4 +8,18 @@ menu = {
 
 # order
 print("Welcome To Python Cafe")
-print(f"Menu:{menu}")
+print("Menu:\n pizza = 250\n burger = 150\n pasta = 200\n salad = 180\n coffee = 100")
+
+# price calculation
+total = 0
+
+# input from user 
+item = input("Enter the item you want to order: ")
+
+# check item is available in menu or not 
+if item in menu:
+    total += menu[item]
+    print(f"{item} added to your order. price: {menu[item]}")
+else:
+    print(f"sorry, {item} is not available in the menu, please choose from the menu")
+
